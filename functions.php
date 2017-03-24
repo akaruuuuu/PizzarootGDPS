71aab9326b59a510f9ad73734ebfbb94915ed87c
akaru <akaruakaridesu@gmail.com>
2017-03-24 17:56:55 +0900

    lazyversion
    yay kms

--- functions.php
+++ functions.php
@@ -21,64 +21,35 @@ function get_client_ip() {
 function makeTime($timestamp) {
 $ts = $timestamp;
 $cts = time();
-$str = "";
+//$str = "";
 $result = $cts-$ts;
 if ($result < 31556952) {
 if ($result < 2629746) {
 if ($result < 86400) {
 if ($result < 3600) {
-if ($result < 60) {
-$n = $result/1;
-if ($n == 1){
-$str = " second";
-}else{
-$str = " seconds";
-}
-$final = $n.$str;
+if ($result < 60) { 
+$final = $result.($result == 1 ? " second" : " seconds");
 }else{
  $n = floor($result/60);
-if ($n == 1){
-$str = " minute";
-  }else{
-  $str = " minutes";
-}
-$final = $n.$str;
+$final = $n.($n == 1? " minute" : " minutes");
  }
             }else{
             $n = floor($result/3600);
-            if ($n == 1){
-                    $str = " hour";
-                    }else{
-                    $str = " hours";
-                    }
-                    $final = $n.$str;
+                    $final = $n.($n == 1? " hour" : " hours");
             }
         }else{
         $n = floor($result/86400);
-        if ($n == 1){
-                    $str = " day";
-                    }else{
-                    $str = " days";
-                    }
-                    $final = $n.$str;
+        
+                    $final = $n.($n == 1? " day" : " days");
         }
     }else{
     $n = floor($result/2629746);
-    if ($n == 1){
-                    $str = " month";
-                    }else{
-                    $str = " months";
-                    }
-                    $final = $n.$str;
+    
+                    $final = $n.($n == 1? " month" : " months");
     }
 }else{
 $n = floor($result/31556952);
-if ($n == 1){
-                    $str = " year";
-                    }else{
-                    $str = " years";
-                    }
-                    $final = $n.$str;
+                    $final = $n.($n == 1? " year" : " years");
 }
 return $final;
 }
